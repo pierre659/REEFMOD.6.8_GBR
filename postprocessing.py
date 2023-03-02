465a65b6618b1273a6c266d70a7748f0c7a2d653
jpaillard <joseph.paillard@etu.minesparis.psl.eu>
2023-03-02 11:58:04 +0100

    post process

--- postprocessing.py
+++ postprocessing.py
@@ -1,31 +1,56 @@
-
-# %%
-import numpy as np
+#%%
 import pandas as pd
-import scipy.io
+from pathlib import Path
 
-# %%
-mat = scipy.io.loadmat('R0_BCA_Moore_MIROC5_45.mat')
 
-meta = mat['META']
-vals = meta
-keys = meta.dtype.descr
-meta_dic = {}
-# Assemble the keys and values into variables with the same name as that used in MATLAB
-for i in range(len(keys)):
-    key = keys[i][0]
-    # squeeze is used to covert matlat (1,n) arrays into numpy (1,) arrays.
-    val = np.squeeze(vals[key][0][0])
-    meta_dic[key] = val
+#%%
+repot = Path('/c/Users/josep/Documents/Mines/crise/REEFMOD.6.8_GBR')
+pd.read_csv(str(repot / ))
 
-reef_ID_list = meta_dic['reef_ID']
+#%%
 
-gps_coords = pd.read_csv('GBR_REEF_POLYGONS_2022.csv')
-gps_coords.set_index('Reef_ID', inplace=True)
-gps_coords = gps_coords.iloc[reef_ID_list]
 
-# %%
-coral_cover_per_taxa = mat['coral_cover_per_taxa'].mean(axis=0).sum(axis=-1)
-for i, idx in enumerate(reef_ID_list):
-    gps_coords[reef_ID_list[i]]['coral_cover'] = coral_cover_per_taxa[i]
-# %%
+json_root = {     'type': 'FeatureCollection',     'crs': {         'type': 'name',         'properties': {             'name': 'urn:ogc:def:crs:OGC:1.3:CRS84'         }     },     'meta': {         'name': OUTPUT_NAME,     } }  for t in range(output_meta['n_timesteps']):     path = JSON_OUTPATH + f't{t}.geojson'     feature_list = []     for reef in range(output_meta['n_reefs']):         feature_list.append({             'type': 'Feature',             'properties': {                 'reef_id': reef,                 'label': OUTPUT_NAME,                 'radius': 1000. * np.random.rand(),                 'value': outputs_df[t][reef],             },             'geometry': {                 'type': 'Point',                 'coordinates': [lat[reef], lon[reef]]             }         })      json_root['features'] = feature_list     json_root['meta']['time_step'] = t     json_root['meta']['IPCC_scenario'] = 'IPCC_scenario_name'     with open(path, 'w') as out:         json.dump(json_roo
+# build JSON :
+# root level : a list of length n_timesteps
+#   level 1 : a list of objects of the form
+#   {'x': longitude, 'y': latitude, 'label': OUTPUT_NAME, 'value': a float value}
+json_root = {
+    'type': 'FeatureCollection',
+    'crs': {
+        'type': 'name',
+        'properties': {
+            'name': 'urn:ogc:def:crs:OGC:1.3:CRS84'
+        }
+    },
+    'meta': {
+        'name': OUTPUT_NAME,
+    }
+}
+
+nyears = len(outputs_df.DHW[0])
+
+for t in range(nyears):
+    t_str = t + 1985
+    path = JSON_OUTPATH + f't{t_str}.geojson'
+    feature_list = []
+    for reef in outputs_df.Reef_ID:
+        feature_list.append({
+            'type': 'Feature',
+            'properties': {
+                'reef_id': reef,
+                'label': OUTPUT_NAME,
+                'radius': 1000. * np.random.rand(),
+                'value': outputs_df.DHW[t],
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [lon[reef], lat[reef]]
+            }
+        })
+
+    json_root['features'] = feature_list
+    json_root['meta']['time_step'] = t
+    json_root['meta']['IPCC_scenario'] = 'IPCC_scenario_name'
+    with open(path, 'w') as out:
+        json.dump(json_root, out, indent=2) 
